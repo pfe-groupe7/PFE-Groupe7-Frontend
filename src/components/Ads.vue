2cffa9fb89f68d51530ea4a2c0efe0e8d59abe9f
Abdulrazzak-ipl <abdulrssak@gmail.com>
2021-12-12 13:49:10 +0100

    first try to fetch

--- src/components/Ads.vue
+++ src/components/Ads.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
+  <div class="post">
+    <div class="loading" v-if="loading">Chargement...</div>
     <div class="card-image">
       <figure class="image is-4by3">
-        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+        <img
+          src="https://bulma.io/images/placeholders/1280x960.png"
+          alt="Placeholder image"
+        />
       </figure>
     </div>
     <div class="card-content">
@@ -11,12 +15,22 @@
           <p class="title is-4">{{ ad.title }}</p>
         </div>
         <div>
-          <button class="button is-small" :title="removeFromFavouriteLabel" v-show="ad.isFavourite" @click="removeFromFavourite(ad.id)">
+          <button
+            class="button is-small"
+            :title="removeFromFavouriteLabel"
+            v-show="ad.isFavourite"
+            @click="removeFromFavourite(ad.id)"
+          >
             <span class="icon is-small">
               <i class="fas fa-heart"></i>
             </span>
           </button>
-          <button class="button is-small" :title="addToFavouriteLabel" v-show="!ad.isFavourite" @click="saveToFavorite(ad.id)">
+          <button
+            class="button is-small"
+            :title="addToFavouriteLabel"
+            v-show="!ad.isFavourite"
+            @click="saveToFavorite(ad.id)"
+          >
             <span class="icon is-small">
               <i class="far fa-heart"></i>
             </span>
@@ -26,15 +40,16 @@
       <div class="content is-clearfix">
         <p>{{ ad.description }}</p>
         <p class="is-pulled-right">
-          <span class="title is-4"><strong>&euro; {{ ad.price }}</strong></span>
+          <span class="title is-4"
+            ><strong>&euro; {{ ad.price }}</strong></span
+          >
         </p>
       </div>
       <div class="card-footer btn-actions">
         <div class="card-footer-item field is-grouped">
           <div class="buttons">
-            <button class="button is-text" >Contactez le vendeur</button>
+            <button class="button is-text">Contactez le vendeur</button>
           </div>
-          
         </div>
       </div>
     </div>
@@ -47,8 +62,8 @@
           title: ad.title,
           price: ad.price,
           description: ad.description,
-          location: ad.location
-        }
+          location: ad.location,
+        },
       }"
     >
     </nuxt-link>
@@ -57,17 +72,23 @@
 
 <script>
 export default {
-  name: 'products',
-  props: ['product'],
-  data () {
+  name: "products",
+  props: ["product"],
+  data() {
     return {
-      addToCartLabel: 'Add to cart',
-      viewDetailsLabel: 'Details',
+      loading: false,
+      addToCartLabel: "Add to cart",
+      viewDetailsLabel: "Details",
       selected: 1,
-      quantityArray: []
-    }
+      quantityArray: [],
+    };
   },
-  mounted () {
+  created() {
+    // récupérer les données lorsque la vue est créée et
+    // que les données sont déjà observées
+    this.fetchData();
+  },
+  mounted() {
     for (let i = 1; i <= 20; i++) {
       this.quantityArray.push(i);
     }
@@ -76,58 +97,56 @@ export default {
     }
   },
   computed: {
-    isUserLogged () {
+    isUserLogged() {
       return this.$store.getters.isUserLoggedIn;
-    }
+    },
   },
   methods: {
-   
-    saveToFavorite (id) {
+    saveToFavorite(id) {
       let isUserLogged = this.$store.state.userInfo.isLoggedIn;
       if (isUserLogged) {
-        this.$store.commit('addToFavourite', id);
+        this.$store.commit("addToFavourite", id);
       } else {
-        this.$store.commit('showLoginModal', true);
+        this.$store.commit("showLoginModal", true);
       }
     },
-    removeFromFavourite (id) {
-      this.$store.commit('removeFromFavourite', id);
+    removeFromFavourite(id) {
+      this.$store.commit("removeFromFavourite", id);
     },
-    onSelectQuantity (id) {
+    onSelectQuantity(id) {
       let data = {
         id: id,
-        quantity: this.selected
-      }
-      this.$store.commit('quantity', data);
-    }
-  }
-}
+        quantity: this.selected,
+      };
+      this.$store.commit("quantity", data);
+    },
+  },
+};
 </script>
 
 <style >
- .details {
-    cursor: pointer;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-    
- }
- .button,
- .select {
-   z-index: 2;
- }
- .select {
-   position: absolute;
-   right: 15px;
-   bottom: 35px;
- }
- .card-content {
-   padding: 0;
- }
- .buttons {
-   margin: 0;
- }
+.details {
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+.button,
+.select {
+  z-index: 2;
+}
+.select {
+  position: absolute;
+  right: 15px;
+  bottom: 35px;
+}
+.card-content {
+  padding: 0;
+}
+.buttons {
+  margin: 0;
+}
 </style>
