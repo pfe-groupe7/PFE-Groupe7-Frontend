2d304a4f7e5511b2ae26c3510ef4bce0af6dae83
ikbouali <ikram.bouali@student.vinci.be>
2021-12-14 00:39:47 +0100

    add photos

--- src/components/DetailAd.vue
+++ src/components/DetailAd.vue
@@ -5,15 +5,30 @@
         </div> 
         <div class="row">
             <div class="col-7">
-                <figure class="img-fluid">
-                    <img  src="https://bulma.io/images/placeholders/480x480.png">
-                </figure> 
+                <div class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item">
+                            <img class="d-block w-100"  src="https://bulma.io/images/placeholders/480x480.png">
+                        </div> 
+                    </div> 
+                    <div class="slider">
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                    </div>
+                </div> 
             </div>
             <div class="col-5">
 
                 
                 <div class="card-content__text"><p>Description</p></div> 
-                    <span class="title is-3"><strong>prix  €</strong></span>
+                <span class="title is-3"><strong>prix  € / Gratuit</strong></span>
+                <div class="card-content__text"><p>Lieu</p></div> 
 
                 </div>
             </div>
