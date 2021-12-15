6f39d3ee909d5d5f5b982fbfbd4c00d0452f9560
ikbouali <ikram.bouali@student.vinci.be>
2021-12-15 17:31:36 +0100

    slider fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -39,7 +39,8 @@
             <b-carousel-slide class="card" v-for="ad in filterdList" v-bind:key="ad.id">
             <p class="captionTitre"> {{ ad.title }} </p>
             <img class="d-block rounded" :src="getMedia(ad.id)">
-            <p class="prix"><strong>{{ ad.price }} €</strong></p>
+            <p class="prix"><span v-if="ad.price== 0" > <strong> Gratuit </strong></span>
+              <span v-else> <strong> {{ ad.price }} € </strong></span></p>
             <a :href="'/detailAd/'+ad.id" class="btnDetail">Voir détails</a>
             </b-carousel-slide>
 
